e3ea5b594bb012ede7e33743b393fb20a87101b7
Richard Zeidler <49378556+Zenzor33@users.noreply.github.com>
2022-11-02 21:08:06 +0000

    MVP

--- scraper.js
+++ scraper.js
@@ -1,6 +1,35 @@
 /*
-
+querySelectors:
+'.is-pct-play-75 > a'
+'.is-pct-play-50 > a'
+'.is-pct-play-25 > a'
+'.is-pct-play-0 > a' <-- for players out
 */
+
+const arrTags = [
+  ".is-pct-play-75 > a",
+  ".is-pct-play-50 > a",
+  ".is-pct-play-25 > a",
+];
+
+let dict = {
+  // indicates probability of playing
+  P75: "",
+  P50: "",
+  P25: "",
+};
+
+for (let i = 0; i < arrTags.length; i++) {
+  console.log(`i: ${i}, ${arrTags[i]}`);
+  const nl2 = document.querySelectorAll(arrTags[i]);
+  const elementsList = Array.from(nl2);
+  let arr = [];
+  elementsList.forEach((node) => arr.push(node.attributes.title.value));
+  const uniq = [...new Set(arr)];
+  if (i === 0) dict["P75"] = uniq;
+  if (i === 1) dict["P50"] = uniq;
+  if (i === 2) dict["P25"] = uniq;
+}
 
 const nl2 = document.querySelectorAll(".is-pct-play-50 > a");
 const elementsList = Array.from(nl2);

4f42257fa630e20364e825550513e7399c0cf48b
Richard Zeidler <49378556+Zenzor33@users.noreply.github.com>
2022-11-06 12:35:43 +0000

    deleted stupid stuff

--- node-scraper-v4/index.js
+++ node-scraper-v4/index.js
@@ -1,9 +1,20 @@
-const axios = require("axios");
-const cheerio = require("cheerio");
-const express = require("express");
-const ObjectsToCsv = require("objects-to-csv");
+// const axios = require("axios");
+// const cheerio = require("cheerio");
+// const express = require("express");
+// const ObjectsToCsv = require("objects-to-csv");
+// const path = require("path");
+import axios from "axios";
+import cheerio from "cheerio";
+// import express from "express";
+import ObjectsToCsv from "objects-to-csv";
+// import path from "path";
+
+import { translateAthleteName } from "./player-dictionary.mjs";
+
 const PORT = 3025;
 const url = "https://www.rotowire.com/basketball/nba-lineups.php";
+
+console.log("awefaef");
 
 const arrTags = [
   ".is-pct-play-75 > a",
@@ -21,10 +32,11 @@ axios(url).then((response) => {
   const $ = cheerio.load(html);
   for (let i = 0; i < arrTags.length; i++) {
     $(arrTags[i], html).each(function () {
-      const athlete = $(this).text();
-      if (i === 0) arr.push({ probabilityToPlay: 75, athlete: athlete });
-      if (i === 1) arr.push({ probabilityToPlay: 50, athlete: athlete });
-      if (i === 2) arr.push({ probabilityToPlay: 25, athlete: athlete });
+      const athleteName = $(this).text();
+      const translatedAthleteName = translateAthleteName(athleteName);
+      if (i === 0) arr.push({ probabilityToPlay: 75, athlete: athleteName });
+      if (i === 1) arr.push({ probabilityToPlay: 50, athlete: athleteName });
+      if (i === 2) arr.push({ probabilityToPlay: 25, athlete: athleteName });
     });
   }
   for (let i = 0; i < arr.length; i++) {
@@ -33,6 +45,17 @@ axios(url).then((response) => {
     if (isUnique(entry.athlete)) uniqueIds.push(entry);
   }
   console.log(uniqueIds);
+
+  // If you use "await", code must be inside an asynchronous function:
+  (async () => {
+    const csv = new ObjectsToCsv(uniqueIds);
+
+    // Save to file:
+    await csv.toDisk("qPlayers.csv");
+
+    // Return the CSV file as string:
+    console.log(await csv.toString());
+  })();
 });
 
 function isUnique(playerName) {

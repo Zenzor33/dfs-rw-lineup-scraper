2755abdbfde7ce166d8f007d624b6fe3351d7754
Richard Zeidler <49378556+Zenzor33@users.noreply.github.com>
2022-11-16 07:03:12 -0300

    file-system-v2 MVP

--- node-scraper-v4/file-system-v2.mjs
+++ node-scraper-v4/file-system-v2.mjs
@@ -21,21 +21,31 @@ const paths = [
 ];
 
 // add conditional for if oldPath does not exist
-const testTransfer = (k, v) => {
+const moveFiles = (k, v) => {
   // k = oldPath
   // v = newPath
-  if (!fs.existsSync(k)) {
-    console.log(`File ${k} does not exist`);
-  } else {
-    fs.rename(k, v, function () {
-      console.log(`Transferred ${k} to ${v}`);
-    });
-  }
+  !fs.existsSync(k)
+    ? console.log(`File ${k} does not exist`)
+    : fs.rename(k, v, function () {
+        console.log(`Transferred ${k} to ${v}`);
+      });
+
+  //   if (!fs.existsSync(k)) {
+  //     console.log(`File ${k} does not exist`);
+  //   } else {
+  //     fs.rename(k, v, function () {
+  //       console.log(`Transferred ${k} to ${v}`);
+  //     });
+  //   }
 };
 
-paths.map((path) => {
-  let key = Object.keys(path);
-  let val = path[key];
-  testTransfer(key[0], val);
-  return;
-});
+function transferFiles() {
+  paths.map((path) => {
+    let key = Object.keys(path);
+    let val = path[key];
+    moveFiles(key[0], val);
+    return;
+  });
+}
+
+transferFiles();
